0e46f1ad9d7aae7ca3883b9ab08a98237fa0474e
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 11:43:06 +1100

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+	const chars = str.split("");
+	let output = "";
 
+	// traverse array of chars in reverse to assemble reversed string 
+	
+	for (let i = chars.length - 1; i >= 0; i--) {
+		output += chars[i];
+	}
+	return output;
 };
 
 // Do not edit below this line
