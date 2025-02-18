98c5cf22da5286f503a14aace6496e473dbe8b2f
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 12:07:09 +1100

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+	let output = []
+
+	// only add numbers to output that are not in remove
+	outer: for (num of arr) {
+		for (removeNum of remove) {
+			if (removeNum === num) continue outer;
+		}
+		output.push(num);
+	}
+	return output;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
