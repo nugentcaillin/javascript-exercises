f1561d16656db60c9b139bdbd1ec12ab782e079a
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 12:19:49 +1100

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(bound1, bound2) {
+	
+	if (bound1 < 0 || bound2 < 0) return "ERROR";
 
+	if (Math.floor(bound1) !== bound1 || 
+		Math.floor(bound2) !== bound2) return "ERROR";
+
+	if (typeof bound1 !== "number" || 
+		typeof bound2 !== "number" ) return "ERROR";
+
+
+
+	
+	let start = Math.min(bound1, bound2);
+	let end = Math.max(bound1, bound2);
+	let sum = 0;
+
+	
+	do {
+		sum += start;
+		start++;
+	} while (start <= end);
+
+	return sum;
 };
 
 // Do not edit below this line
