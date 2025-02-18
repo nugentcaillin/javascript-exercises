744decc18c0d09f3392e65b24fb31a54e8b234b9
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 12:32:02 +1100

    exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	// leap years are divisible by 4.
+	// if they are divisible by 100, they must also be divisible by 400
+	if (year % 100 === 0 && year % 400 !== 0) return false;
+	if (year % 4 === 0 ) return true;
+	return false;
 };
 
 // Do not edit below this line
