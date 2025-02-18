29d65724ad4325892c764ca53106285d15cb8417
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 11:31:23 +1100

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+	if (count < 0) return "ERROR";
+	let output = "";
+	for (let i = 0; i < count; i++) {
+		output += str;
+		console.log(output);
+	}
+	return output;
 };
 
 // Do not edit below this line
