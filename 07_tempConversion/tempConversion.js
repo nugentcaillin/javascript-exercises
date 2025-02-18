c2e2e72c19a8b1662aab93d6ae94882ac308632b
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-18 12:43:38 +1100

    exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+	// formula to convert farenheit to celsius
+	// is (farenheit - 32) * 5 /9
+	const OFFSET = -32;
+	const RATIO = 5/9;
+
+	let celsius = (farenheit + OFFSET) * RATIO;
+
+	// convert to one decimal place
+	return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+	// formula to convert celsius to farenheit is 
+	// celsius * 9/5 + 32
+	const OFFSET = 32;
+	const RATIO = 9 / 5;
+	
+	let farenheit = celsius * RATIO + OFFSET;
+
+	// convert to one decimal place
+	return +farenheit.toFixed(1);
 };
 
 // Do not edit below this line
