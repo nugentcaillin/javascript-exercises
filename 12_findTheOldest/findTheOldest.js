e1f758e129724afdf31cb35a0d3f4651a28fd6e7
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-20 20:18:39 +1100

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(person) {
+	const birthYear = person.yearOfBirth;
+	const latestYearAlive = person["yearOfDeath"]
+		? person["yearOfDeath"]
+		: new Date().getFullYear();
+	return latestYearAlive - birthYear;
+}
 
+const findTheOldest = function(people) {
+	return people.reduce((accumulator, current) => {
+		if (getAge(current) > getAge(accumulator)) return current;
+		return accumulator;
+	})
 };
 
 // Do not edit below this line
