368f98d866eae64f14b3d02cc09fe8e7af0cdd58
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-20 18:53:13 +1100

    Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {	
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((accumulator, current) => accumulator * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+	for (let i = 2; i <= a; i++) {
+		total *= i;
+	} 
+	return total;
 };
 
 // Do not edit below this line
