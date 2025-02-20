67a614b39abc5887169e55411a3aa952db87c0f9
Caillin Nugent <nugentcaillin@gmail.com>
2025-02-20 19:08:40 +1100

    exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	
+	// trim all non alphanumeric and make lower case
+	str = str.toLowerCase().replace(/[^a-z0-9]|\s/g, "");
 
+	// two pointer approach
+	let left = 0;
+	let right = str.length - 1;
+	while (left < right) {
+		if (str.charAt(left) !== str.charAt(right)) return false;
+		left++;
+		right--;
+	}
+	return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
